6d94bde5e4c4cbc3d170a1a43312d4fb409860ec
JonnyCash <jonathanacp93@gmail.com>
2019-10-21 16:18:27 -0500

    updates level 4

--- level_4/vote.sh
+++ level_4/vote.sh
@@ -8,20 +8,18 @@ url="http://158.69.76.135/level4.php"
 
 id=892
 
-user_agent='Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'
+user_agen='Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'
 referer="Referer: http://158.69.76.135/level4.php"
 
 for i in {1..98}
 do
 echo -e "${RED}------Vote for ${id}------${NC}"
-proxy=$(curl http://pubproxy.com/api/proxy?type=http | grep -o '"ipPort":.*' | cut -d '"' -f4 | cut -d '"' -f1)
-echo "hey"
-echo $proxy
-cookie=$(curl -x $proxy -c cookie.txt -I -s -H "$referer" -A "$user_agent" $url | grep -o 'HoldTheDoor=.*' | cut -d ';' -f1 | cut -d '=' -f2)
+echo -e 'AUTHENTICATE ""\r\nsignal NEWNYM\r\nQUIT' | nc 127.0.0.1 9051
+cookie=$(torify curl -c cookie.txt -I -s -H "$referer" -A "$user_agent" $url | grep -o 'HoldTheDoor=.*' | cut -d ';' -f1 | cut -d '=' -f2)
 
-curl -x $proxy -H "$referer" -A "$user_agent" --cookie cookie.txt -X POST $url -d "key=$cookie&holdthedoor=&id=$id" #> /dev/null
+torify curl -H "$referer" -A "$user_agent" --cookie cookie.txt -X POST $url -d "key=$cookie&holdthedoor=&id=$id" #> /dev/null
 
 echo -e "${BLU}Send Post ... ${NC}"
 
-#sleep 10
+sleep 5
 done
